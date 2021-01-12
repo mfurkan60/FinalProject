39315eeca01aa504f04093f817222a94b25960ae
Mert ERGÜDEN <mfurkanergdn@hotmail.com>
2021-01-13 02:04:44 +0300

    Update Firebase import Code

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
     ImageView logo;
     TextView  forgotPass;
